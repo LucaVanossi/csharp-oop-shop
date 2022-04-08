d2371861eb6090ab100b2e8c2658e3063f42b6ce
LucaVanossi <vanossi.luca13@gmail.com>
2022-04-08 17:03:44 +0200

    Add project files.

---
+++ C-Sharp shop/Prodotto.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_shop
+{
+    public class Prodotto
+    {
+        private int Codice;
+        private string Nome;
+        private string Descrizione;
+        private double Prezzo;
+        private double Iva;
+
+        public Prodotto(string Nome, string Descrizione, double Prezzo, double Iva)
+        {
+            this.Codice = GeneraProdotto();
+            this.Nome = Nome;
+            this.Descrizione = Descrizione;
+            this.Prezzo = Prezzo;
+            this.Iva = Iva;
+        }
+        private int GeneraProdotto()
+        {
+            Random NumeroCasuale = new Random();
+            return NumeroCasuale.Next(10000);
+        }
+        private string GetNome()
+        {
+            return Nome;
+        }
+
+        private string GetDescrizione()
+        {
+            return Descrizione;
+        }
+
+        private double GetPrezzo()
+        {
+            return Prezzo;
+        }
+
+        private double GetIva()
+        {
+            return Iva;
+        }
+
+        private double PrezzoSenzaIva()
+        {
+            return (Prezzo - Iva);
+            
+        }
+        
+        private string NomeEsteso ()
+        {
+            return (Codice.ToString() + " - " + Nome);
+        }
+        
+        public string VisualizzaDati()
+        {
+            string InfoProdotto = (NomeEsteso() + Environment.NewLine + "Il codice del prodotto è: " + Codice + Environment.NewLine + 
+                "Il nome del prodotto è: " + Nome + Environment.NewLine + "Descrizione: " + Descrizione
+                + Environment.NewLine + "Il Prezzo netto è: " + PrezzoSenzaIva() + Environment.NewLine 
+                + "Iva: " + Iva + Environment.NewLine + "Prezzo compreso d'Iva: " + Prezzo);
+            return InfoProdotto;
+        }
+
+
+    }
+}
---
+++ C-Sharp shop/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using C_Sharp_shop;
+
+Prodotto Moto = new Prodotto("Honda", "ADV 350", 6990, 1537.80);
+
+Console.WriteLine(Moto.VisualizzaDati());
+
